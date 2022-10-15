7ce2e54cfe830fa0db9f5e8daa9d1d5d644e6c46
ZaharDomolego <domolegozahar@gmail.com>
2022-10-16 00:19:26 +0800

    fixed map appearing

--- src/components/content/AppointmentCard.js
+++ src/components/content/AppointmentCard.js
@@ -9,28 +9,18 @@ import { routes } from '../../globalConst';
 
 const AppointmentCard = () => {
     const location = useLocation();
-    const getComp =()=>{
-        let comp=null
-        if(location.pathname!=routes.main){
-            if(window.innerWidth<=1000){
-                comp = <Map/>
-            }   
-        }
-        return comp;
-    }
+
     return(
         <div id="Appointment-card" className="d-flex w-100">
             <div className="d-flex flex-column col-5">
                 <AppointmentNav/>
-                {
-                    location.pathname != routes.main?
-                    <AppointmentContent />
-                    :
-                    null
-                }
+                <AppointmentContent />
             </div>
             {
-                getComp()
+                window.innerWidth > 1000 || location.pathname == routes.main?
+                <Map />
+                :
+                null
             }
         </div>
     )
