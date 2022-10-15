e1cb6cd65b95dea1d711a3f6e0725536d06d86f4
ZaharDomolego <domolegozahar@gmail.com>
2022-10-16 03:55:07 +0800

    fixed table

--- src/components/content/AppointmentContent.js
+++ src/components/content/AppointmentContent.js
@@ -9,30 +9,32 @@ const AppointmentContent = () => {
         const items =[];
         for(let i=0;i<100;i++){
             if(i==0){
-                items.push(<HistoryListItem itemprops={{
+                items.push(<HistoryListItem key={ `head_item_${i}` } itemprops={{
                 
-                    dateTime:<th className=''>{"Начало/Конец работы"}</th>,  
-                    transport:<th className=''>{"ТС и номер"}</th>,
-                    workType:<th className=''>{"Тип работы"}</th>,
-                    fullname:<th className=''>{"ФИО ответсвенного"}</th>
+                    dateTime:<th>{"Начало/Конец работы"}</th>,  
+                    transport:<th>{"ТС и номер"}</th>,
+                    workType:<th>{"Тип работы"}</th>,
+                    fullname:<th>{"ФИО ответсвенного"}</th>
                 }}/>)
             }
-            items.push(<HistoryListItem itemprops={{
+            items.push(<HistoryListItem key={ `list_item_${i}` } itemprops={{
                 
                 dateTime:<td className='pe-5'>{ "19.02.2020"} <br/>{"12.09.2022"}</td>,  
                 transport:<td className='ps-5 pe-4'>{"nissan Gtd"}<br/>{"О888ОО/88"}</td>,  
-                workType:<td className=''>{"Drag Racing"}</td>,
-                fullname:<td className=''>{"Зубенко Михаил Петрович"}</td>
+                workType:<td>{"Drag Racing"}</td>,
+                fullname:<td>{"Зубенко Михаил Петрович"}</td>
             }}/>)
         }
         return items
     }
     return(
         <table bordercolor="white" border="1" id="Appointment-content" className="mt-4 d-flex flex-column ms-3 me-3 p-2">
+            <tbody>
             {
                 location.pathname==routes.history? 
                     generator():null
             }
+            </tbody>
         </table>
     )
 }
