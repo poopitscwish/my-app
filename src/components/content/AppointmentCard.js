bcb1442f4ed561582372993c5b6676b382c0967b
ZaharDomolego <domolegozahar@gmail.com>
2022-10-15 13:39:28 +0800

    configured map

--- src/components/content/AppointmentCard.js
+++ src/components/content/AppointmentCard.js
@@ -4,7 +4,7 @@ import '../../style/appointment-card.css';
 
 const AppointmentCard = () => {
     return(
-        <div id="Appointment-card" className="d-flex m-auto p-3">
+        <div id="Appointment-card" className="d-flex p-3 col-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
             laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
--- src/components/content/Map.js
+++ src/components/content/Map.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { YMaps, Map as YaMap } from 'react-yandex-maps';
 
+import '../../style/map.css';
+
 const Map = () => {
     return(
         <YMaps>
-            <div id="Map" className="ms-4 d-flex">
-                <YaMap defaultState={{ center: [56.01528, 92.89325], zoom: 9 }}  width="100%" height="600px" />
+            <div id="Map" className="w-100">
+                <YaMap defaultState={{ center: [56.01528, 92.89325], zoom: 9 }} width="100%" height="100%" />
             </div>
         </YMaps>
     )
